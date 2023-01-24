6a98608ddbaf30ba78ea16090c60e6c71e46eab9
ganeshneelekani <ganeshneelekani@gmail.com>
2023-01-24 20:50:03 +0530

    first commit

---
+++ src/four_clojure/easy/core.clj
@@ -0,0 +1,166 @@
+(ns four-clojure.easy.core
+  (:require [clojure.walk :as walk]
+             [clojure.string :as str]))
+
+; Problem 19, Last Element
+(= (last [1 2 3 4 5]) 5)
+
+(= (last '(5 4 3)) 3)
+(= (last ["b" "c" "d"]) "d")
+
+; Problem 20, Penultimate Element
+
+(= (last (butlast (list 1 2 3 4 5))) 4)
+(= (last (butlast ["a" "b" "c"])) "b")
+(= (last (butlast [[1 2] [3 4]])) [1 2])
+
+; Problem 21, Nth Element
+
+(= (nth '(4 5 6 7) 2) 6)
+(= (nth [:a :b :c] 0) :a)
+(= (nth [1 2 3 4] 1) 2)
+(= (nth '([1 2] [3 4] [5 6]) 2) [5 6])
+
+; Problem 22, Count a Sequence
+(= (count '(1 2 3 3 1)) 5)
+(= (count "Hello World") 11)
+(= (count [[1 2] [3 4] [5 6]]) 3)
+(= (count '(13)) 1)
+(= (count '(:a :b :c)) 3)
+
+; Problem 23, Reverse a Sequence
+(= (reverse [1 2 3 4 5]) [5 4 3 2 1])
+(= (reverse (sorted-set 5 7 2 7)) '(7 5 2))
+(= (reverse [[1 2] [3 4] [5 6]]) [[5 6] [3 4] [1 2]])
+
+; Problem 24, Sum It All Up
+(= (reduce + [1 2 3]) 6)
+(= (reduce + (list 0 -2 5 5)) 8)
+(= (reduce + #{4 2 1}) 7)
+(= (reduce + '(0 0 -1)) -1)
+(= (reduce + '(1 10 3)) 14)
+
+; Problem 26, Fibonacci Sequence
+(defn -fib 
+  ([]
+   (-fib 1 1))
+  ([a b]
+   (cons a (lazy-seq (-fib b (+ a b))))))
+
+(defn get-fib [n]
+  (take n (-fib)))
+
+(= (get-fib 3) '(1 1 2))
+(= (get-fib 6) '(1 1 2 3 5 8))
+(= (get-fib 8) '(1 1 2 3 5 8 13 21))
+
+; Problem 27, Palindrome Detector
+(defn -polindrome [coll]
+  (let [r (fn []
+            (reduce #(conj %1 %2) `() coll))]
+    (= coll (if (string? coll)
+              (apply str (r))
+              (r)))))
+
+(false? (-polindrome '(1 2 3 4 5)))
+(true? (-polindrome "racecar"))
+(true? (-polindrome [:foo :bar :foo]))
+(true? (-polindrome '(1 1 3 3 1 1)))
+(false? (-polindrome '(:a :b :c)))
+
+; Problem 28, Flatten a Sequence 
+; Special Restrictions : flatten
+(defn -pre-walk [coll]
+  (filter (complement sequential?) 
+          (rest (tree-seq sequential? seq coll))))
+
+(= (-pre-walk '(1 2 3 [4 [5 6]])) '(1 2 3 4 5 6))
+(= (-pre-walk ["a" ["b"] "c"]) '("a" "b" "c"))
+(= (-pre-walk '((((:a))))) '(:a))
+
+; Problem 29, Get the Caps
+(defn capital-letter [coll]
+  (apply str (reduce (fn [acc x]
+                       (if (Character/isUpperCase (first x))
+                         (conj acc x)
+                         acc))
+                     []
+                     (str/split coll #""))))
+
+(= (capital-letter "HeLlO, WoRlD!") "HLOWRD")
+(empty? (capital-letter "nothing"))
+(= (capital-letter "$#A(*&987Zf") "AZ")
+
+; Problem 30, Compress a Sequence
+(defn -partition-by
+  [f coll]
+  (lazy-seq (when-let [s (seq coll)]
+              (let [fst (first s)
+                    fv (f fst)
+                    run (cons fst (take-while #(= fv %) (next s)))]
+                (cons run (-partition-by f (drop (count run) s)))))))
+
+(defn -occurance [coll]
+   (let [f (fn [](map (fn [x]
+                      (first x)) (-partition-by identity coll)))]
+     (if (string? coll)
+       (apply str (f))
+       (f))))
+
+(= (apply str (-occurance "Leeeeeerrroyyy")) "Leroy")
+(= (-occurance [1 1 2 3 3 2 2 3]) '(1 2 3 2 3))
+(= (-occurance [[1 2] [1 2] [3 4] [1 2]]) '([1 2] [3 4] [1 2]))
+
+;; Problem 31, Pack a Sequence
+(= (-partition-by identity [1 1 2 1 1 1 3 3]) '((1 1) (2) (1 1 1) (3 3)))
+(= (-partition-by identity [:a :a :b :b :c]) '((:a :a) (:b :b) (:c)))
+(= (-partition-by identity [[1 2] [1 2] [3 4]]) '(([1 2] [1 2]) ([3 4])))
+
+
+;; Problem 32, Duplicate a Sequence
+(defn duplicate [coll]
+  (mapcat (fn [x]
+         (list x x)) coll))
+
+(= (duplicate [1 2 3]) '(1 1 2 2 3 3))
+(= (duplicate [:a :a :b :b]) '(:a :a :a :a :b :b :b :b))
+(= (duplicate [[1 2] [3 4]]) '([1 2] [1 2] [3 4] [3 4]))
+(= (duplicate [44 33]) [44 44 33 33])
+
+;; Problem 33, Replicate a Sequence
+(defn replicate-seq [coll n]
+  (mapcat (fn [x]
+            (repeat n x)) coll))
+
+(= (replicate-seq [1 2 3] 2) '(1 1 2 2 3 3))
+(= (replicate-seq [:a :b] 4) '(:a :a :a :a :b :b :b :b))
+(= (replicate-seq [4 5 6] 1) '(4 5 6))
+(= (replicate-seq [[1 2] [3 4]] 2) '([1 2] [1 2] [3 4] [3 4]))
+(= (replicate-seq [44 33] 2) [44 44 33 33])
+
+;; Problem 34, Implement range
+;; Special Restrictions : range
+(defn -range [in ln]
+  (reduce #(cons %2 %1) `()  
+          (loop [result []
+                 x      in]
+            (if (= x ln)
+              result
+              (recur (cons x result) (inc x))))))
+
+(= (-range 1 4) '(1 2 3))
+(= (-range -2 2) '(-2 -1 0 1))
+(= (-range 5 8) '(5 6 7))
+
+;; Problem 38, Maximum value
+;; Special Restrictions : max,max-key
+
+(defn -max [& coll]
+  (reduce #(if (> %1 %2) %1 %2) coll))
+
+(= (-max 1 8 3 4) 8)
+(= (-max 30 20) 30)
+(= (-max 45 67 11) 67)
+
+;; Problem 39, Interleave Two Seqs
+;; Special Restrictions : interleave
---
+++ src/four_clojure/elementary/core.clj
@@ -0,0 +1,93 @@
+(ns four-clojure.elementary.core)
+
+; Problem 1, Nothing but the Truth
+(= true true)
+
+; Problem 2, Simple Math
+(= (- 10 (* 2 3)) 4)
+
+; Problem 3, Strings
+(= "HELLO WORLD" (.toUpperCase "hello world"))
+
+; Problem 4, Lists
+(= (list :a :b :c) '(:a :b :c))
+
+; Problem 5, conj on lists
+(= (list 1 2 3 4) (conj '(2 3 4) 1))
+
+; Problem 6, Vectors
+(= [:a :b :c] (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c))
+
+; Problem 7, conj on vectors
+
+(= [ 1 2 3 4] (conj [1 2 3] 4))
+
+(= [ 1 2 3 4] (conj [1 2] 3 4))
+
+; Problem 8, Sets
+(=  #{:a :b :c :d} (set '(:a :a :b :c :c :c :c :d :d)))
+
+(= #{:a :b :c :d} (clojure.set/union #{:a :b :c} #{:b :c :d}))
+
+; Problem 9, conj on sets
+(= #{1 2 3 4} (conj #{1 4 3} 2))
+
+; Problem 10, Maps
+(= 20 ((hash-map :a 10, :b 20, :c 30) :b))
+
+
+(= 20 (:b {:a 10, :b 20, :c 30}))
+
+;; Problem 11, conj on maps
+(= {:a 1, :b 2, :c 3} (conj {:a 1} [:b 2] [:c 3]))
+
+; Problem 12, Sequences
+(= 3 (first '(3 2 1)))
+
+(= 3 (second [2 3 4]))
+
+(= 3 (last (list 1 2 3)))
+
+;Problem 13, rest 
+(= [20 30 40] (rest [10 20 30 40]))
+
+; Problem 14, Functions
+(= 8 ((fn add-five [x] (+ x 5)) 3))
+
+(= 8 ((fn [x] (+ x 5)) 3))
+
+(= 8 (#(+ % 5) 3))
+
+(= 8 ((partial + 5) 3))
+
+; Problem 15, Double Down
+
+(= (+ 2 2) 4)
+(= (+ 3 3) 6)
+(= (+ 11 11) 22)
+(= (+ 7 7) 14)
+
+; Problem 16, Hello World
+(= (#(str "Hello, " % "!") "Dave") "Hello, Dave!")
+(= (#(str "Hello, " % "!") "Jenn") "Hello, Jenn!")
+(= (#(str "Hello, " % "!") "Rhea") "Hello, Rhea!")
+
+; Problem 17, map
+(= (list 6 7 8) (map #(+ % 5) '(1 2 3)))
+
+; Problem 18, filter
+(= (list 6 7) (filter #(> % 5) '(3 4 5 6 7)))
+
+;; Problem 35, Local bindings
+(= 7 (let [x 5] (+ 2 x)))
+(= 7 (let [x 3, y 10] (- y x)))
+(= 7 (let [x 21] (let [y 3] (/ x y))))
+
+;; Problem 36, Let it Be
+(= 10 (let [x 5 y 5] (+ x y)))
+(= 4 (let [y 2 z 2] (+ y z)))
+(= 1 (let [z 1] z))
+
+;; Problem 37, Regular Expressions
+(= "ABC" (apply str (re-seq #"[A-Z]+" "bA1B3Ce ")))
+
